1825dfcf5e9cb3031947f4d17a5f863efeb31f2d
evgeniia lemanova <jane87.05@mail.ru>
2023-05-26 23:38:59 +0300

    add post all

--- catalog/views.py
+++ catalog/views.py
@@ -8,5 +8,5 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
+        print(f'{name}({phone}): {message}')
     return render(request, 'contacts.html')
